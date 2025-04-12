1a9d6760de2cdc97e8919eaacb249633b0476db0
Shikhar Yadav <shikharyadav645@gmail.com>
2025-04-12 13:40:12 +0530

    desktop navigation done

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,6 +4,8 @@ import Image from "@node_modules/next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 function Nav() {
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -13,7 +15,31 @@ function Nav() {
           width={30}
           className="object-contain"
         />
+        <p className="logo_text">PromptBestie</p>
       </Link>
+
+      <div className="sm:flex hidden ">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+              ></Image>
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 }
